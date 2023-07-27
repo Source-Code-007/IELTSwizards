5686a469c87b176742c868948951613c26b6ee73
Source-Code-007 <utsho926@gmail.com>
2023-07-27 18:39:32 +0600

    header added

--- src/Components/Homepage/Header/Banner/Banner.jsx
+++ src/Components/Homepage/Header/Banner/Banner.jsx
@@ -1,13 +1,75 @@
+import bannerImg from '/public/assets/img/banner/bannerRight.png'
+import bannerImgBg from '/public/assets/img/banner/bannerImgBg.png'
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { FaRegCheckCircle } from "react-icons/fa";
 
 
+// TODO: ul list disc modifiy
 const Banner = () => {
+    const [formStatus, setFormStatus] = useState('start')
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+
+    };
+
     return (
-        <div className="min-h-screen bg-green-500 flex items-center justify-center">
-                  <div>
-                    <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, non.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae tenetur officia quod, laudantium, unde eum nostrum ad optio aliquid alias eligendi earum.</p>
-                    <button className="btn btn-primary">Buy now</button>
-                  </div>
+        <div className="h-[93vh] relative overflow-hidden">
+            <div className='h-full px-8 md:px-12 lg:px-16 grid grid-cols-2 items-center'>
+                {/* banner left */}
+                <div className='text-center space-y-4'>
+                    <h2 className="text-[#001E43] text-xl md:text-3xl lg:text-5xl font-bold">Start Your IELTS Preparation <br /> And Get Desire Bands</h2>
+                    <ul className='list-disc text-[#000000] inline-flex flex-col'>
+                        <li>Get access to IELTS Centralised Library</li>
+                        <li>British Council Certified Teacher</li>
+                    </ul>
+
+                    {/* form */}
+                    <form onSubmit={handleSubmit(onSubmit)} className='rounded-[53px] py-6 px-12 w-[635px] mx-auto' style={{ boxShadow: '0px 4px 50px 0px #0D19481F' }}>
+                        {/* status */}
+                        <div className='flex justify-center gap-4'>
+                            <span className='bg-orange-500 rounded flex gap-2 items-center'> <FaRegCheckCircle></FaRegCheckCircle> Start</span>
+                            <span className='bg-orange-500 rounded'>Fill details</span>
+                            <span className='bg-orange-500 rounded'>Confirm</span>
+                        </div>
+
+                        {/* Name and phone number */}
+                        {formStatus === 'start' && <div className='space-y-8'>
+                            <h2 className='my-subtitle text-left'>Free IELTS Classes</h2>
+                            <input className='my-inp' {...register("name", { required: true })} placeholder='Enter your full name' />
+                            {errors.name && <span>This field is required</span>}
+                            <div className='my-3 flex gap-3'>
+                                <input className='my-inp !w-14' {...register("countryCode", { required: true })} placeholder='country' />
+                                <input className='my-inp flex-1' {...register("number", { required: true })} placeholder='Phone number' />
+                                {errors.number && <span>This field is required</span>}
+                            </div>
+                            <button className='my-btn-one w-full' onClick={() => setFormStatus('details')}>Continue</button>
+                        </div>}
+
+                        {/* details */}
+                        {
+                            formStatus === 'details' && <div>
+                                <button className='my-btn-one w-full' onClick={() => setFormStatus('confirm')}>Continue</button>
+                            </div>
+                        }
+
+                        {/* confirm */}
+                        {
+                            formStatus === 'confirm' && <div>
+                                <button type="submit" className='my-btn-one w-full' onClick={() => setFormStatus('')}>Submit</button>
+                            </div>
+                        }
+                        <p>By continuing, you agree to our <span className='text-blue-300 hover:underline'>Term of services</span> & <span className='text-blue-300 hover:underline '>Privacy policy</span></p>
+                    </form>
+                </div>
+
+                {/* banner right */}
+                <div>
+                    <img src={bannerImg} alt="" />
+                </div>
+            </div>
+
+            <img src={bannerImgBg} className='absolute right-0 top-0 -z-30' />
         </div>
     );
 };
--- src/Components/Homepage/Header/Navbar/Navbar.jsx
+++ src/Components/Homepage/Header/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import logo from '/public/assets/img/InGelt-white.png'
 const Navbar = () => {
     return (
-        <div className="flex justify-between py-3  bg-blue-500">
-            <div className='max-w-7xl mx-auto'>
-                <img src={logo} alt="" />
-                <ul className='flex items-center gap-3'>
-                    <li> Home </li>
-                    <li> Home </li>
-                    <li> Home </li>
-                    <li> Home </li>
+        <div className="bg-[#051C45]">
+            <div className='flex justify-between py-2 px-8 md:px-12 lg:px-16'>
+                <img src={logo} alt="IELTS wizards" className='h-14 w-14'/>
+                <ul className='flex items-center gap-3 text-white'>
+                    <li className='cursor-pointer hover:text-orange-500 transition duration-500'> Home </li>
+                    <li className='cursor-pointer hover:text-orange-500 transition duration-500'> About </li>
+                    <li className='cursor-pointer hover:text-orange-500 transition duration-500'> Contact </li>
+                    <li className='cursor-pointer hover:text-orange-500 transition duration-500'> Services </li>
                 </ul>
             </div>
         </div>
